bb9ea3ecfb6e8af003ea27fa0bc8ac341ec9aecd
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-05-14 13:20:25 +0530

    Adding/connecting firebase to app

---
+++ src/firebase.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCNuzy_c14hW6GfNiKS5Inv7-tbUv05wrs",
+    authDomain: "netflix-clone-c6541.firebaseapp.com",
+    projectId: "netflix-clone-c6541",
+    storageBucket: "netflix-clone-c6541.appspot.com",
+    messagingSenderId: "640412368311",
+    appId: "1:640412368311:web:00e5ef48337865780f3c32"
+};
