1a396e0024f70037992e4dd192b1d96e7e96023f
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-05-15 18:16:24 +0530

    initializing firebase

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCNuzy_c14hW6GfNiKS5Inv7-tbUv05wrs",
     authDomain: "netflix-clone-c6541.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "640412368311",
     appId: "1:640412368311:web:00e5ef48337865780f3c32"
 };
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth();
+
+export { auth };
+export default db;
