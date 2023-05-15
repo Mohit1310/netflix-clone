bd40076b57c3f18e3d205bd8c21dfa7da00c61ad
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-05-15 18:18:21 +0530

    using redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
-import { useState } from 'react'
-import HomeScreen from './HomeScreen'
+import { useEffect, useState } from 'react'
+import HomeScreen from './screens/HomeScreen'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './screens/ProfileScreen';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if(userAuth) {
+        // loggged in 
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }));
+      } else {
+        // logged out
+        dispatch(logout());
+      }
+    })
+
+    return unsubscribe;
+  },[dispatch]);
 
   return (
     <div className='app'>
+      {!user ? <LoginScreen /> : (
       <Router>
         <Routes>
           <Route exact path='/' element={<HomeScreen />}/>
-          <Route path='/test' element={<h1>Hello you</h1>} />
+          <Route path='/profile' element={<ProfileScreen />}/>
         </Routes>
       </Router>
+      )}
     </div>
   )
 }
